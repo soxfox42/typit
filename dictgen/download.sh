6b44a02b337912e036491c5cee17159cfa9935ec
CaCO3 <caco@ruinelli.ch>
2022-01-18 20:01:04 +0100

    removed unused files

--- dictgen/download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf scowl
-mkdir scowl
-wget -qO- https://sourceforge.net/projects/wordlist/files/SCOWL/2020.12.07/scowl-2020.12.07.tar.gz/download | tar xvz - -C scowl --strip-components 1
