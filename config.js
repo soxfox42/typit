ae7bc4ef905191cc5f8ba083b338bedf7c45befb
CaCO3 <caco@ruinelli.ch>
2022-01-23 16:37:02 +0100

    fix keyboard layout

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ let keyboardLayout = {
     ["EN"]: ["Q,W,E,R,T,Y,U,I,O,P",
              "A,S,D,F,G,H,J,K,L",
              "Enter,Z,X,C,V,B,N,M,Backspace"],
-    ["DE"]: [ "Q,W,E,R,T,Y,U,I,O,P,Ä",
+    ["DE"]: [ "Q,W,E,R,T,Z,U,I,O,P,Ä",
              "A,S,D,F,G,H,J,K,L,Ö,Ü",
-             "Enter,Z,X,C,V,B,N,M,Backspace"]
+             "Enter,Y,X,C,V,B,N,M,Backspace"]
 }
 
 let keyboardValidation = {
