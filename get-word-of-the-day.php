aabc7b2164d32f723b4a4d812c13c5a7cfbc194d
George Ruinelli <caco3@ruinelli.ch>
2022-03-06 23:58:16 +0100

    extenden log, moved to non public folder

--- get-word-of-the-day.php
+++ get-word-of-the-day.php
@@ -3,9 +3,10 @@
     // IP anomizer (set last octet (IPv4) resp the last 80 bytes (IPv6) to zero
     $_SERVER['REMOTE_ADDR'] = preg_replace(['/\.\d*$/','/[\da-f]*:[\da-f]*$/'],['.0','0000:0000'], $_SERVER['REMOTE_ADDR']);
 
-    $LOG_FOLDER = "./logs";
+    $path = explode("/", __FILE__);
+    $LOG_FOLDER = "/" . $path[1] . "/" . $path[2] . "/wordle-deutsch-logs/";
 
-    // wordes generated with old (javascript mulberry32) algorithm
+    // words generated with old (javascript mulberry32) algorithm
     // To be removed a while after we switched to server side words
     $oldRandomizerAlgorithm = [
         "1645743600" => ["mjnat", 1915], // Fr., 25. Feb.
@@ -64,9 +65,10 @@
         $result = json_decode($response, true);
         $country = $result['country'];
         $region = $result['regionName'];
+        $timezone = $result['offset'] / 3600;
 
-        $log  = date("[Y-m-d, H:i:s] ") . $_GET['timestamp'] . ", " .  "$target, " . $_SERVER['REMOTE_ADDR'] . ", " . "$country, $region, " . $_SERVER['HTTP_USER_AGENT'] . PHP_EOL;
-        file_put_contents('./logs/' . date("Y-m-d") . '.log', $log, FILE_APPEND);
+        $log  = date("[Y-m-d, H:i:s] ") . $_GET['timestamp'] . ", " .  "$target, " . $_SERVER['REMOTE_ADDR'] . ", " . "$timezone, $country, $region, " . $_SERVER['HTTP_USER_AGENT'] . PHP_EOL;
+        file_put_contents($LOG_FOLDER . "/" . date("Y-m-d") . '.log', $log, FILE_APPEND);
 
     }
     else {
